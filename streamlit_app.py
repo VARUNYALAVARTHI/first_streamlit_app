8eb0fae6a128d9d7839d02e3480416305f9cef56
VARUNYALAVARTHI <112621648+VARUNYALAVARTHI@users.noreply.github.com>
2022-09-01 17:52:12 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ streamlit.header('BREAKFAST fava')
 streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
 streamlit.text('🍞Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔Hard-Boiled Free-Range Egg')
-streamlit.text('🥑 Avacado Toast')
+streamlit.text('🥑 Avocado Toast')
 
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
