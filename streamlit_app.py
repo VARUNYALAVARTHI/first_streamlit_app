b888e4d838f25a64e1a9dff764e85f0a074170e2
VARUNYALAVARTHI <112621648+VARUNYALAVARTHI@users.noreply.github.com>
2022-09-01 16:49:17 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,9 @@
 import streamlit
 
-streamlit.title('My parents New Healthy Diner  Breakfast Menu vada ')
+streamlit.title('My parents New Healthy Diner')
+
+
+streamlit.header('BREAKFAST MENU')
+streamlit.text('dosa')
+streamlit.text('vada')
+streamlit.text('idly')
